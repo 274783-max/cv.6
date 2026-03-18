40f5175aeb611a606fdcb53025846b2db9e554a6
Alena Frankova <274783@vutbr.cz>
2026-03-18 10:54:02 +0100

    Updated structure

---
+++ src/circles_project/Circles_stats.py
@@ -0,0 +1,21 @@
+
+
+
+
+circle_1 = {"x": x1, "y": y1, "r": r1}
+circle_2 = {"x": x2, "y": y2, "r": r2}
+
+def radius_sum(r1, r2):
+    rad = r1 + r2
+    return rad
+
+def euclid_distance(x1, y1, x2, y2):
+    d = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2)
+    return d
+def has_intersection(circle_1, circle_2):
+    if d > rad:
+
+    its =
+    return("intersects": its)
+
+# {"intersects": True, "intersections_count": 2}
