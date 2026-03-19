57b966aeaad07d84f57270e28bc8f7fb08b4cbe6
Alena Franková <274783@vutbr.cz>
2026-03-20 00:37:53 +0100

    Drobne zmeny

--- src/circles_project/Circles_stats.py
+++ src/circles_project/Circles_stats.py
@@ -1,7 +1,6 @@
 
 
 
-
 circle_1 = {"x": x1, "y": y1, "r": r1}
 circle_2 = {"x": x2, "y": y2, "r": r2}
 
@@ -12,13 +11,14 @@ def radius_sum(r1, r2):
 def euclid_distance(x1, y1, x2, y2):
     d = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2)
     return d
+
 def has_intersection(circle_1, circle_2):
     if d > rad:
-        its = 0
-    if d == rad:
-        its = 1
-    if d < rad:
-        its = 2
-    return("intersects": its)
+        sec = 0
+    else d == rad:
+        sec = 1
+    elif d < rad:
+        sec = 2
+    return sec
 
 # {"intersects": True, "intersections_count": 2}
